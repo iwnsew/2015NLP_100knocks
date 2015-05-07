2eb83a7d6f58873a567f3f10da4c7b2bd27c6c82
hightensan <hightensan@gmail.com>
2015-05-07 17:19:01 +0900

    07. テンプレートによる文生成
    引数x, y, zを受け取り「x時のyはz」という文字列を返す関数を実装せよ．さらに，x=12, y="気温",
    z=22.4として，実行結果を確認せよ．

---
+++ 第1章/7.py
@@ -0,0 +1,20 @@
+#coding: utf-8
+
+def template(x,y,z):
+	print x,"の時の",y,"は",z
+
+print "x時のyはz"
+print "xは？:",
+_x = raw_input()
+if _x.isdigit() == True:
+ 	_x = int(_x)
+print "yは？:",
+_y = raw_input()
+if _y.isdigit() == True:
+ 	_y = int(_y)
+print "zは？:",
+_z = raw_input()
+if _z.isdigit() == True:
+ 	_z = int(_z)
+
+template(_x,_y,_z)
