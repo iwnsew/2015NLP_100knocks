23b936d5e9d22abe4c382c4199865ed41add5cdc
hightensan <hightensan@gmail.com>
2015-09-08 19:27:17 +0900

    動詞の格フレーム情報の抽出
    45のプログラムを改変し，述語と格パターンに続けて項（述語に係っている文節そのもの）をタブ区切り形式で出力せよ．45の仕様に加えて，以下の仕様を
    満たすようにせよ．
    
    項は述語に係っている文節の単語列とする（末尾の助詞を取り除く必要はない）
    述語に係る文節が複数あるときは，助詞と同一の基準・順序でスペース区切りで並べる
    「吾輩はここで始めて人間というものを見た」という例文（neko.txt.cabochaの8文目）を考える．
    この文は「始める」と「見る」の２つの動詞を含み，「始める」に係る文節は「ここで」，「見る」に係る文節は「吾輩は」と「ものを」と解析された場合は，
    次のような出力になるはずである．

---
+++ 第5章/nlp_46.py
@@ -0,0 +1,28 @@
+# coding:utf-8
+import nlp_41
+
+#############		計算量がばかでかい←要改善					###################
+#############		UNIXを使った出力ができていない		###################
+def main():
+	all_chunk_list = nlp_41.make_chunk()
+	for chunk_list in all_chunk_list:
+		for chunk_element in chunk_list:
+			verb = ""; particle = ""; sentence = ""
+			for mor in chunk_element.morphs:
+				if mor.pos == "動詞":
+					verb += mor.base
+					break # 最左の動詞の基本形を述語とするため
+			# 動詞にかかる文節の助詞(格)の出力
+			for num in chunk_element.srcs:
+				for mor in chunk_list[num].morphs:
+					if mor.pos == "助詞":
+						particle += mor.surface
+						particle += " "
+						for morph in chunk_list[num].morphs:
+							sentence += morph.surface
+						sentence += " "
+			if verb != "" and particle != "":
+				print verb,"\t",particle,"\t",sentence
+
+if __name__ == "__main__":
+	main()
